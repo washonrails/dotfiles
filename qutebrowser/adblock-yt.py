070e42bf43390e770d74504754872f39e38a7b1e
Wallace Henrique <98850074+washonrails@users.noreply.github.com>
2022-12-15 23:08:34 -0300

    dotfiles

---
+++ qutebrowser/adblock-yt.py
@@ -0,0 +1,19 @@
+
+"""Block YT ads.
+
+I DO NOT USE THIS SCRIPT ANYMORE, in favor of a real adblocker."""
+
+
+from qutebrowser.api import interceptor
+
+
+def filter_yt(info: interceptor.Request):
+	"""Block the given request if necessary."""
+	url = info.request_url
+	if (url.host() == 'www.youtube.com' and
+			url.path() == '/get_video_info' and
+			'&adformat=' in url.query()):
+		info.block()
+
+
+interceptor.register(filter_yt)
